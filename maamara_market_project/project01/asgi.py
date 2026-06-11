c72207568ef4b7b1fddb480a29d9cb247198dc67
Maa Mara <mcsimenc@gmail.com>
2026-06-11 14:10:02 +0000

    Remove docker config files, update .gitignore

--- maamara_market_project/project01/asgi.py
+++ maamara_market_project/project01/asgi.py
@@ -4,6 +4,7 @@ from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 import oder.chanels  # <-- now this has websocket_urlpatterns
 from .middleware.jwt_auth import JWTAuthMiddleware
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project01.settings")
 
