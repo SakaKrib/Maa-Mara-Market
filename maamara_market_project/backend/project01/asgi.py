e729fef271795a01cad82479250e6ad72b86c16e
SakaKrib <sakahaoke@gmail.com>
2026-09-18 13:11:24 +0300

    Wire centralized realtime websocket system

--- maamara_market_project/backend/project01/asgi.py
+++ maamara_market_project/backend/project01/asgi.py
@@ -2,32 +2,17 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project01.settings")
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-#from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from project01.middleware.jwt_auth import JWTAuthMiddleware
+from core.routing import websocket_urlpatterns as core_websocket_urlpatterns
+from oder.routing import websocket_urlpatterns as order_websocket_urlpatterns
 
 django_asgi_app = get_asgi_application()
 
-import oder.chanels  # <-- now this has websocket_urlpatterns
-from .middleware.jwt_auth import JWTAuthMiddleware
-
+websocket_urlpatterns = core_websocket_urlpatterns + order_websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": JWTAuthMiddleware(
-        URLRouter(
-            oder.chanels.websocket_urlpatterns
-        )
-    ),
+    "http": django_asgi_app,
+    "websocket": JWTAuthMiddleware(URLRouter(websocket_urlpatterns)),
 })
-
-
-
-#application = ProtocolTypeRouter({
-#    "http": get_asgi_application(),
-#    "websocket": JWTAuthMiddleware(
-#        URLRouter(
-#            oder.chanels.websocket_urlpatterns
-#        )
-#    ),
-#})
