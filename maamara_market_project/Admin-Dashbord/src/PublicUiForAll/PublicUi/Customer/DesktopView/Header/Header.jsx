2d0096d5b13b1a020cf3ab2069a534cb27c44d31
SakaKrib <sakahaoke@gmail.com>
2026-09-18 20:23:45 +0300

    Remove duplicate category navigation row

--- maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Header/Header.jsx
+++ maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Header/Header.jsx
@@ -73,7 +73,6 @@ const Header = () => {
             <Link to="/blogs">Journal</Link>
             <Link to="/organic">Organic</Link>
           </nav>
-          <CategoryNavigation />
         </div>
       </div>
 
