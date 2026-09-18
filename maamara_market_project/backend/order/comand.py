4e532b8da9f8a7223ef9f3de537fa1537f696b16
SakaKrib <sakahaoke@gmail.com>
2026-09-18 19:39:06 +0300

    Rename OderItem references to OrderItem

--- maamara_market_project/backend/order/comand.py
+++ maamara_market_project/backend/order/comand.py
@@ -1,8 +1,8 @@
 from django.core.management.base import BaseCommand
-from .models import Order, OderItem
+from .models import Order, OrderItem
 
 class Command(BaseCommand):
-    help = 'Migrate OderItem M2M order to ForeignKey'
+    help = 'Migrate OrderItem M2M order to ForeignKey'
 
     def handle(self, *args, **kwargs):
         orders = Order.objects.all()
@@ -10,4 +10,4 @@ class Command(BaseCommand):
             for item in order.items.all():  # existing M2M
                 item.order = order
                 item.save()
-        self.stdout.write(self.style.SUCCESS('OderItem order field populated successfully.'))
+        self.stdout.write(self.style.SUCCESS('OrderItem order field populated successfully.'))
