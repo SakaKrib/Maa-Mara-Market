cb9375fefb26a948387ed9b27af9022f1dfa40fd
SakaKrib <sakahaoke@gmail.com>
2026-09-18 20:45:27 +0300

    Simplify storefront homepage

--- maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Main/Main.jsx
+++ maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Main/Main.jsx
@@ -1,39 +1,20 @@
 import React from "react";
 import "../../../../PublicUi/maamara.css";
-import "../../../../../index.css";
 import HomepageHero from "./HomepageHero";
-import ListPage from "./ListPage";
-import TrendingProducts from "./Trending/Product";
-import BrandList from "./Trending/Brands";
-import Banners from "./Trending/Banner";
 import CategoryWithItems from "./Trending/Category/CategoryWithItemList";
-import AdvertBlogs from "../../../../../cmponents/Hooks/BlogHooksNew/BlogAdvert";
-import OrganicSlideshow from "./Trending/OrganicAdvert/OrganicDvert";
 import MarketplaceDiscovery from "./Discovery/MarketplaceDiscovery";
-import MultiCollections from "./Trending/MultiCollections";
 
 const Main = () => (
   <main className="mm-page">
     <HomepageHero />
 
     <section className="category-collections mm-section">
-      <div className="category-cont">
-        <div className="containers">
-          <div className="wrappe">
-            <CategoryWithItems />
-          </div>
-        </div>
+      <div className="mm-container">
+        <CategoryWithItems />
       </div>
     </section>
 
-    <BrandList />
     <MarketplaceDiscovery />
-    <MultiCollections />
-    <ListPage />
-    <OrganicSlideshow />
-    <TrendingProducts />
-    <Banners />
-    <AdvertBlogs />
   </main>
 );
 
