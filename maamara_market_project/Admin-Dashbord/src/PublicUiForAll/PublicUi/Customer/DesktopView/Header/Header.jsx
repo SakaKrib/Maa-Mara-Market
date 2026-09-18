b9d5dc7959f3fcc97db3d203a0ab9b9df3549785
SakaKrib <sakahaoke@gmail.com>
2026-09-19 02:21:04 +0300

    Remove desktop header action icons

--- maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Header/Header.jsx
+++ maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Header/Header.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Heart, MapPin, ShoppingBag, UserRound, Menu, Search, Sparkles, ChevronDown, Globe2 } from "lucide-react";
+import { Heart, ShoppingBag, UserRound, Menu, Search, Sparkles, ChevronDown, Globe2 } from "lucide-react";
 import "../../../../PublicUi/maamara.css";
 import SearchBar from "../../../Navigations/Search/Search";
-import NavIcons from "../../../Navigations/Search/NavIcons/NavIcon";
 import { Link } from "react-router-dom";
 import useNewBlogs from "../../../../../cmponents/Hooks/BlogHooksNew/NewBlogs";
 import Maamara from "../../../../../assets/Logo/Maamara.jpg";
@@ -87,10 +86,6 @@ const Header = () => {
             <SearchBar />
           </div>
             </div>
-
-          <div className="mm-header-actions">
-            <NavIcons />
-          </div>
         </div>
       </div>
 
