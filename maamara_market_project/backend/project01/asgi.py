8203b821717d2d8d6813edba2d60582b4d39c342
SakaKrib <sakahaoke@gmail.com>
2026-09-18 19:23:44 +0300

    Fix ASGI Django app initialization order

--- maamara_market_project/backend/project01/asgi.py
+++ maamara_market_project/backend/project01/asgi.py
@@ -4,11 +4,15 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project01.settings")
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+
+django_asgi_app = get_asgi_application()
+
+# Import Django/DRF-dependent middleware and routing only after Django's
+# application registry has been initialized. Importing JWTAuthentication
+# before get_asgi_application() raises AppRegistryNotReady during Daphne startup.
 from project01.middleware.jwt_auth import JWTAuthMiddleware
 from core.routing import websocket_urlpatterns as core_websocket_urlpatterns
 from oder.routing import websocket_urlpatterns as order_websocket_urlpatterns
-
-django_asgi_app = get_asgi_application()
 
 websocket_urlpatterns = core_websocket_urlpatterns + order_websocket_urlpatterns
 
