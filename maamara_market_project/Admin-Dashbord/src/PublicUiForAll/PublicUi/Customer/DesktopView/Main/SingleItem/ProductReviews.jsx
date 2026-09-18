539e49dde94d214d633bb8469035ef6b96624993
SakaKrib <sakahaoke@gmail.com>
2026-09-18 18:11:14 +0300

    Refactor storefront component: ProductReviews.jsx

---
+++ maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Main/SingleItem/ProductReviews.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReviewSection from "../Review";
+
+const ProductReviews = ({ item }) => (
+  <section>
+    <ReviewSection item={item} />
+  </section>
+);
+
+export default ProductReviews;
