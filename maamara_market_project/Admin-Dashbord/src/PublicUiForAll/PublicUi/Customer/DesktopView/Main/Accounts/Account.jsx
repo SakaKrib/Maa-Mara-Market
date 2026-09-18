64fbb37caca83c5e32069ea9932d931239466edb
SakaKrib <sakahaoke@gmail.com>
2026-09-18 18:38:22 +0300

    Clean unused account icon import

--- maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Main/Accounts/Account.jsx
+++ maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Main/Accounts/Account.jsx
@@ -5,7 +5,6 @@ import { Button } from "../../../../../../../components/ui/button";
 import { Input } from "../../../../../../../components/ui/input";
 import {
   User,
-  Mail,
   Coins,
   Gift,
   Edit2,
