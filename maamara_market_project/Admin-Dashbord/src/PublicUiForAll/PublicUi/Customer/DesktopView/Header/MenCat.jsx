d959815042cc161d43478148a42cb65f02098e2f
SakaKrib <sakahaoke@gmail.com>
2026-09-18 20:24:59 +0300

    Remove legacy hardcoded category menu product previews

--- maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Header/MenCat.jsx
+++ maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Customer/DesktopView/Header/MenCat.jsx
@@ -110,28 +110,6 @@ export default function MegaMenuMen() {
             </div>
           </div>
 
-          {/* Static product preview */}
-          <div className="flexcol products">
-            <div className="flex flex-col">
-              <div className="media">
-                <div className="thumbnail object-cover">
-                  <a href="#">
-                    <img
-                      src="products/kuba wall hanging.jpg"
-                      alt="Kuba Wall Hanging"
-                    />
-                  </a>
-                </div>
-              </div>
-              <div className="text-content">
-                <h4>Most Viewed</h4>
-                <a href="#" className="primary-button">
-                  Add to cart
-                </a>
-              </div>
-            </div>
-          </div>
-
         </div>
       </div>
     </div>
