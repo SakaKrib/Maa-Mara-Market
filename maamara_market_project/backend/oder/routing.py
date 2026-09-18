6503bd8ee21ab71bbbc010c2154da8dbc23fa94c
SakaKrib <sakahaoke@gmail.com>
2026-09-18 13:12:29 +0300

    Consolidate existing order websocket routes

--- maamara_market_project/backend/oder/routing.py
+++ maamara_market_project/backend/oder/routing.py
@@ -1,10 +1,22 @@
 from django.urls import re_path
-from . import consumers
+from .consumers import (
+    OrderConsumer,
+    CustomerConsumer,
+    PayoutConsumer,
+    StockConsumer,
+    VendorOrdersConsumer,
+    VendorNotificationsConsumer,
+    ActivityLogsConsumer,
+)
+from vendorDashboard.consumers import VendorPayoutConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/orders/(?P<order_id>\w+)/$', consumers.OrderConsumer.as_asgi()),
-    re_path(r"ws/customers/(?P<vendor_user_id>\w+)/$", consumers.CustomerConsumer.as_asgi()),
-
-     # ⭐ NEW: payout websocket
-    re_path(r'ws/payout/(?P<reference>\w+)/$', consumers.PayoutConsumer.as_asgi()),
+    re_path(r"ws/orders/(?P<order_id>\\w+)/$", OrderConsumer.as_asgi()),
+    re_path(r"ws/customers/(?P<vendor_user_id>\\w+)/$", CustomerConsumer.as_asgi()),
+    re_path(r"ws/stock/(?P<vendor_id>\\d+)/$", StockConsumer.as_asgi()),
+    re_path(r"ws/payout/(?P<reference>[\\w-]+)/$", VendorPayoutConsumer.as_asgi()),
+    re_path(r"ws/payout-status/(?P<reference>[\\w-]+)/$", PayoutConsumer.as_asgi()),
+    re_path(r"ws/vendor-orders/$", VendorOrdersConsumer.as_asgi()),
+    re_path(r"ws/vendor-notifications/$", VendorNotificationsConsumer.as_asgi()),
+    re_path(r"ws/activity-logs/$", ActivityLogsConsumer.as_asgi()),
 ]
