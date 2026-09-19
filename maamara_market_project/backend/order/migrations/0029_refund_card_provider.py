a0e7ddaf6c3196a10901ffa5ce3b6426dbe2adad
SakaKrib <sakahaoke@gmail.com>
2026-09-19 15:33:31 +0300

    Fix order migration app-label dependencies

--- maamara_market_project/backend/order/migrations/0029_refund_card_provider.py
+++ maamara_market_project/backend/order/migrations/0029_refund_card_provider.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("order", "0028_refund_ledger"),
+        ("oder", "0028_refund_ledger"),
     ]
 
     operations = [
