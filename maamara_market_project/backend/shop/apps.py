55cc46b5139e3b3379b01e78c4fb476c585e1b56
SakaKrib <sakahaoke@gmail.com>
2026-09-20 21:26:09 +0300

    Load shop moderation real-time signals

--- maamara_market_project/backend/shop/apps.py
+++ maamara_market_project/backend/shop/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class LoginsignConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shop'
+
+    def ready(self):
+        from . import realtime_signals  # noqa: F401
