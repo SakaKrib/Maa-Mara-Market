476c635f4b7dc37268e1a04d5c961355f07caa12
SakaKrib <sakahaoke@gmail.com>
2026-09-20 21:15:13 +0300

    Broadcast vendor directory changes over WebSockets

---
+++ maamara_market_project/backend/vendorDashboard/signals.py
@@ -0,0 +1,34 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+
+from .models import Vendor
+
+
+def _broadcast_vendor_change(action, vendor_id):
+    channel_layer = get_channel_layer()
+    if not channel_layer:
+        return
+
+    async_to_sync(channel_layer.group_send)(
+        "admin_vendors",
+        {
+            "type": "vendor_changed",
+            "action": action,
+            "vendor_id": vendor_id,
+        },
+    )
+
+
+@receiver(post_save, sender=Vendor)
+def vendor_saved(sender, instance, created, **kwargs):
+    _broadcast_vendor_change(
+        "created" if created else "updated",
+        instance.pk,
+    )
+
+
+@receiver(post_delete, sender=Vendor)
+def vendor_deleted(sender, instance, **kwargs):
+    _broadcast_vendor_change("deleted", instance.pk)
