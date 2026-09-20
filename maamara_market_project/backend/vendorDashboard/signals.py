3579969eefa9ab02d0f4137594fba24642734971
SakaKrib <sakahaoke@gmail.com>
2026-09-20 21:25:44 +0300

    Broadcast vendor request changes after database commit

--- maamara_market_project/backend/vendorDashboard/signals.py
+++ maamara_market_project/backend/vendorDashboard/signals.py
@@ -1,43 +1,90 @@
 from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
 from django.db import transaction
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+from channels.layers import get_channel_layer
 
-from .models import Vendor
+from .models import Vendor, VendorRequest, VendorItemRequest, PriceChangeRequest
 
 
-def _broadcast_vendor_change(action, vendor_id):
+def _broadcast(group, resource, action, object_id):
     channel_layer = get_channel_layer()
     if not channel_layer:
         return
 
-    try:
-        async_to_sync(channel_layer.group_send)(
-            "admin_vendors",
-            {
-                "type": "vendor_changed",
-                "action": action,
-                "vendor_id": vendor_id,
-            },
-        )
-    except Exception:
-        # Real-time delivery must never make vendor persistence fail.
-        return
+    def send():
+        try:
+            async_to_sync(channel_layer.group_send)(
+                group,
+                {
+                    "type": "request_changed",
+                    "resource": resource,
+                    "action": action,
+                    "object_id": object_id,
+                },
+            )
+        except Exception:
+            # WebSocket delivery must never break database persistence.
+            return
+
+    transaction.on_commit(send)
+
+
+def _broadcast_vendor_change(action, vendor_id):
+    _broadcast("admin_vendors", "vendor", action, vendor_id)
+
+
+def _broadcast_request_change(resource, action, object_id):
+    _broadcast("admin_vendor_requests", resource, action, object_id)
 
 
 @receiver(post_save, sender=Vendor)
 def vendor_saved(sender, instance, created, **kwargs):
-    transaction.on_commit(
-        lambda: _broadcast_vendor_change(
-            "created" if created else "updated",
-            instance.pk,
-        )
-    )
+    _broadcast_vendor_change("created" if created else "updated", instance.pk)
 
 
 @receiver(post_delete, sender=Vendor)
 def vendor_deleted(sender, instance, **kwargs):
-    transaction.on_commit(
-        lambda: _broadcast_vendor_change("deleted", instance.pk)
+    _broadcast_vendor_change("deleted", instance.pk)
+
+
+@receiver(post_save, sender=VendorRequest)
+def vendor_request_saved(sender, instance, created, **kwargs):
+    _broadcast_request_change(
+        "vendor",
+        "created" if created else "updated",
+        instance.pk,
     )
+
+
+@receiver(post_save, sender=VendorItemRequest)
+def vendor_item_request_saved(sender, instance, created, **kwargs):
+    _broadcast_request_change(
+        "item",
+        "created" if created else "updated",
+        instance.pk,
+    )
+
+
+@receiver(post_save, sender=PriceChangeRequest)
+def price_change_request_saved(sender, instance, created, **kwargs):
+    _broadcast_request_change(
+        "price",
+        "created" if created else "updated",
+        instance.pk,
+    )
+
+
+@receiver(post_delete, sender=VendorRequest)
+def vendor_request_deleted(sender, instance, **kwargs):
+    _broadcast_request_change("vendor", "deleted", instance.pk)
+
+
+@receiver(post_delete, sender=VendorItemRequest)
+def vendor_item_request_deleted(sender, instance, **kwargs):
+    _broadcast_request_change("item", "deleted", instance.pk)
+
+
+@receiver(post_delete, sender=PriceChangeRequest)
+def price_change_request_deleted(sender, instance, **kwargs):
+    _broadcast_request_change("price", "deleted", instance.pk)
