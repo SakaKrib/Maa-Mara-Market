386d1b1b21de2015f01b5f5bdba223f8f814e84e
SakaKrib <sakahaoke@gmail.com>
2026-09-20 17:04:23 +0300

    Register careers in Django admin

--- maamara_market_project/backend/shop/admin.py
+++ maamara_market_project/backend/shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Review, Reaction, Banner, ChatFile, BlogPost, CommentBlog, ReactionBlog, Wishlist
+from .models import Review, Reaction, Banner, ChatFile, BlogPost, CommentBlog, ReactionBlog, Wishlist, CareerVacancy, JobApplication
 
 # Register your models here.
 
@@ -30,3 +30,6 @@ admin.site.register(Wishlist)
 # admin.site.register(OrganicCategory, OrganicCategoryAdmin)
 # admin.site.register(OrganicSubCategory, OrganicSubCategoryAdmin)
 
+
+admin.site.register(CareerVacancy)
+admin.site.register(JobApplication)
