3cbbb3715be85a149f934704f274678593368244
SakaKrib <sakahaoke@gmail.com>
2026-09-20 10:13:45 +0300

    Fix MobileMenu missing useEffect import

--- maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Navigations/Footer/MobileFooter.jsx
+++ maamara_market_project/Admin-Dashbord/src/PublicUiForAll/PublicUi/Navigations/Footer/MobileFooter.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../../../Services/Api";
 import { TrendingUp, UserRound, Heart, Search, ShoppingCart } from "lucide-react";
