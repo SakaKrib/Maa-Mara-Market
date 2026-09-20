52e77f2cfb3e53d6c314dfab2563647cf3d1349c
SakaKrib <sakahaoke@gmail.com>
2026-09-20 21:15:19 +0300

    Load vendor real-time signals when Django starts

--- maamara_market_project/backend/vendorDashboard/apps.py
+++ maamara_market_project/backend/vendorDashboard/apps.py
@@ -1,7 +1,9 @@
 from django.apps import AppConfig
 
-class VendordashboardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vendorDashboard'
 
-    
+class VendordashboardConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "vendorDashboard"
+
+    def ready(self):
+        from . import signals  # noqa: F401
