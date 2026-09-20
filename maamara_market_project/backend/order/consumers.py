f8c0a9d06e6ba2824c648bc73c728b4b91c3601b
SakaKrib <sakahaoke@gmail.com>
2026-09-20 23:31:54 +0300

    Add realtime admin accounts consumer

--- maamara_market_project/backend/order/consumers.py
+++ maamara_market_project/backend/order/consumers.py
@@ -454,3 +454,29 @@ class ActivityLogsConsumer(AsyncWebsocketConsumer):
 
 # Admin activity
 
+
+
+class AdminAccountsConsumer(AsyncWebsocketConsumer):
+    """Real-time ledger/account invalidation channel for admin users."""
+
+    group_name = "admin_accounts"
+
+    async def connect(self):
+        user = self.scope.get("user")
+        if not user or not user.is_authenticated or not user.is_staff:
+            await self.close(code=4403)
+            return
+
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def account_changed(self, event):
+        await self.send(text_data=json.dumps({
+            "type": "account.changed",
+            "resource": event.get("resource", "transaction"),
+            "action": event.get("action", "updated"),
+            "object_id": event.get("object_id"),
+        }))
