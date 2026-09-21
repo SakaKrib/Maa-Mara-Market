d0c2809343998b1fa5cf13c9f397da6f4172e3f7
SakaKrib <sakahaoke@gmail.com>
2026-09-21 06:16:36 +0300

    Protect messaging WebSockets with allowed host validation

--- maamara_market_project/backend/project01/asgi.py
+++ maamara_market_project/backend/project01/asgi.py
@@ -4,6 +4,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project01.settings")
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 django_asgi_app = get_asgi_application()
 
@@ -18,5 +19,7 @@ websocket_urlpatterns = core_websocket_urlpatterns + order_websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": JWTAuthMiddleware(URLRouter(websocket_urlpatterns)),
+    "websocket": AllowedHostsOriginValidator(
+        JWTAuthMiddleware(URLRouter(websocket_urlpatterns))
+    ),
 })
