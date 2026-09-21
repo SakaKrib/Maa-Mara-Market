12af04501a82e452298796fb0978753f6c6b9d66
SakaKrib <sakahaoke@gmail.com>
2026-09-21 06:04:26 +0300

    Tighten rich text sanitizer allowlist

--- maamara_market_project/Admin-Dashbord/src/utils/sanitizeRichText.js
+++ maamara_market_project/Admin-Dashbord/src/utils/sanitizeRichText.js
@@ -1,19 +1,18 @@
 import DOMPurify from "dompurify";
 
 const RICH_TEXT_CONFIG = {
-  USE_PROFILES: { html: true },
   ALLOWED_TAGS: [
     "p", "br", "strong", "em", "u", "s",
     "h1", "h2", "h3",
     "ul", "ol", "li",
     "blockquote", "pre", "code", "a",
   ],
-  ALLOWED_ATTR: ["href", "title", "target", "rel", "style"],
-  ALLOWED_URI_REGEXP: /^(?:(?:https?|mailto):|[^a-z]|[a-z+.-]+(?:[^a-z+.-:]|$))/i,
+  ALLOWED_ATTR: ["href", "title", "target", "rel"],
   FORBID_TAGS: ["script", "style", "iframe", "object", "embed", "form", "svg", "math"],
   FORBID_ATTR: [
     "onerror", "onload", "onclick", "onmouseover", "onfocus",
-    "onmouseenter", "onmouseleave", "style",
+    "onmouseenter", "onmouseleave", "oninput", "onsubmit",
+    "style", "srcdoc",
   ],
   SANITIZE_DOM: true,
   SANITIZE_NAMED_PROPS: true,
