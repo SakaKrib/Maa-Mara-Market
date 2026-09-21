844c73c0675fc9574c44c666066d5f462c2e82b1
SakaKrib <sakahaoke@gmail.com>
2026-09-21 06:52:37 +0300

    Remove unnecessary CSS sanitizer dependency

--- maamara_market_project/backend/shop/sanitizers.py
+++ maamara_market_project/backend/shop/sanitizers.py
@@ -1,5 +1,4 @@
 import bleach
-from bleach.css_sanitizer import CSSSanitizer
 
 RICH_TEXT_TAGS = {
     "p", "br", "strong", "em", "u", "s",
@@ -14,10 +13,6 @@ RICH_TEXT_ATTRIBUTES = {
 
 RICH_TEXT_PROTOCOLS = {"http", "https", "mailto"}
 
-RICH_TEXT_CSS = CSSSanitizer(
-    allowed_css_properties={"text-align"}
-)
-
 
 def sanitize_rich_text(value):
     """Return a strictly allow-listed HTML fragment safe for rich-text rendering."""
@@ -29,7 +24,6 @@ def sanitize_rich_text(value):
         tags=RICH_TEXT_TAGS,
         attributes=RICH_TEXT_ATTRIBUTES,
         protocols=RICH_TEXT_PROTOCOLS,
-        css_sanitizer=RICH_TEXT_CSS,
         strip=True,
         strip_comments=True,
     )
