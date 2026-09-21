6e4ca93db5990987c489e17535891c159aa11196
SakaKrib <sakahaoke@gmail.com>
2026-09-21 05:41:39 +0300

    Route realtime messaging WebSocket

--- maamara_market_project/backend/core/routing.py
+++ maamara_market_project/backend/core/routing.py
@@ -1,6 +1,8 @@
 from django.urls import re_path
 from .consumers import RealtimeConsumer
+from .messaging_consumer import MessagingConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/realtime/$", RealtimeConsumer.as_asgi()),
+    re_path(r"ws/messaging/$", MessagingConsumer.as_asgi()),
 ]
