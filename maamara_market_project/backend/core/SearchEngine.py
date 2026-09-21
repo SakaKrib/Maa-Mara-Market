82916c477fe1012968a1cbebd5f96e0094fc4b7e
SakaKrib <sakahaoke@gmail.com>
2026-09-21 21:32:12 +0300

    Complete lightweight search serializer imports

--- maamara_market_project/backend/core/SearchEngine.py
+++ maamara_market_project/backend/core/SearchEngine.py
@@ -3,12 +3,12 @@ from operator import add
 
 from django.core.paginator import EmptyPage, Paginator
 from django.db.models import Avg, Case, Count, IntegerField, Q, Sum, Value, When
+from rest_framework import serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from ReactSerializers.models import Item
-from .Serializer import ItemSerializer
 from order.views import IsAuthenticatedOrVisitor
 
 
