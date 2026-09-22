5a87cc956a16d0d80985bf85ecc755027dd51734
SakaKrib <sakahaoke@gmail.com>
2026-09-23 00:42:16 +0300

    Fix item draft API object lookup import

--- maamara_market_project/backend/vendorDashboard/views.py
+++ maamara_market_project/backend/vendorDashboard/views.py
@@ -17,6 +17,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from PIL import Image
 
 from ReactSerializers.Serializers import VendorPayoutSerializer
