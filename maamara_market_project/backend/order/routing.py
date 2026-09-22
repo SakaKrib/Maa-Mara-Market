2b95684a8e96870989bfa20a6aeeae60e92d97b8
SakaKrib <sakahaoke@gmail.com>
2026-09-22 16:34:58 +0300

    Fix remaining stock WebSocket route regex

--- maamara_market_project/backend/order/routing.py
+++ maamara_market_project/backend/order/routing.py
@@ -20,7 +20,7 @@ from .currency_consumer import CurrencyConsumer
 websocket_urlpatterns = [
     re_path(r"ws/orders/(?P<order_id>\w+)/$", OrderConsumer.as_asgi()),
     re_path(r"ws/customers/(?P<vendor_user_id>\w+)/$", CustomerConsumer.as_asgi()),
-    re_path(r"ws/stock/(?P<vendor_id>\\d+)/$", StockConsumer.as_asgi()),
+    re_path(r"ws/stock/(?P<vendor_id>\d+)/$", StockConsumer.as_asgi()),
     re_path(r"ws/payout/(?P<reference>[\w-]+)/$", VendorPayoutConsumer.as_asgi()),
     re_path(r"ws/payout-status/(?P<reference>[\w-]+)/$", PayoutConsumer.as_asgi()),
     re_path(r"ws/vendor-orders/$", VendorOrdersConsumer.as_asgi()),
