8411ac0ba43e65b52ee526ab528fad67f1571f8b
SakaKrib <sakahaoke@gmail.com>
2026-09-22 21:40:40 +0300

    Restore sales chart prominence and fix shop button

--- maamara_market_project/Admin-Dashbord/src/cmponents/VENDORPAGE/SellerDasboard/Seller.jsx
+++ maamara_market_project/Admin-Dashbord/src/cmponents/VENDORPAGE/SellerDasboard/Seller.jsx
@@ -154,20 +154,20 @@ const Home = ({ vendor_id }) => {
             />
           </article>
 
-          <article className={`${cardClass} xl:col-span-7`}>
+          <article className={`${cardClass} min-h-[360px] xl:col-span-8`}>
             <BarVendor />
           </article>
 
-          <article className={`${cardClass} xl:col-span-5`}>
+          <article className={`${cardClass} xl:col-span-4`}>
             <Activities />
           </article>
         </div>
       </section>
 
-      <div className="sticky bottom-4 z-30 flex justify-center pt-3">
+      <div className="pointer-events-none fixed inset-x-0 bottom-4 z-40 flex justify-center px-4">
         <Link
           to="/"
-          className="inline-flex min-h-11 items-center justify-center rounded-full bg-black px-6 py-3 text-sm font-semibold text-white shadow-lg transition hover:bg-[#262626] focus:outline-none focus:ring-2 focus:ring-black/20"
+          className="pointer-events-auto inline-flex min-h-11 items-center justify-center rounded-full bg-black px-6 py-3 text-sm font-semibold text-white shadow-lg ring-1 ring-black/10 transition hover:bg-[#262626] focus:outline-none focus:ring-2 focus:ring-black/20"
         >
           Go To Shop
         </Link>
