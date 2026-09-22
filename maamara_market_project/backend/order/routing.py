f96b64763cb6ed1736beee0c27f80f252cfd5406
SakaKrib <sakahaoke@gmail.com>
2026-09-22 16:34:28 +0300

    Fix WebSocket route regex matching

--- maamara_market_project/backend/order/routing.py
+++ maamara_market_project/backend/order/routing.py
@@ -18,11 +18,11 @@ from vendorDashboard.consumers import (
 from .currency_consumer import CurrencyConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/orders/(?P<order_id>\\w+)/$", OrderConsumer.as_asgi()),
-    re_path(r"ws/customers/(?P<vendor_user_id>\\w+)/$", CustomerConsumer.as_asgi()),
+    re_path(r"ws/orders/(?P<order_id>\w+)/$", OrderConsumer.as_asgi()),
+    re_path(r"ws/customers/(?P<vendor_user_id>\w+)/$", CustomerConsumer.as_asgi()),
     re_path(r"ws/stock/(?P<vendor_id>\\d+)/$", StockConsumer.as_asgi()),
-    re_path(r"ws/payout/(?P<reference>[\\w-]+)/$", VendorPayoutConsumer.as_asgi()),
-    re_path(r"ws/payout-status/(?P<reference>[\\w-]+)/$", PayoutConsumer.as_asgi()),
+    re_path(r"ws/payout/(?P<reference>[\w-]+)/$", VendorPayoutConsumer.as_asgi()),
+    re_path(r"ws/payout-status/(?P<reference>[\w-]+)/$", PayoutConsumer.as_asgi()),
     re_path(r"ws/vendor-orders/$", VendorOrdersConsumer.as_asgi()),
     re_path(r"ws/vendor-notifications/$", VendorNotificationsConsumer.as_asgi()),
     re_path(r"ws/admin/vendors/$", VendorDirectoryConsumer.as_asgi()),
