3fb3bcdc127bbb23aac4e9917018217f30f27d39
SakaKrib <sakahaoke@gmail.com>
2026-09-22 15:06:45 +0300

    Centralize vendor sidebar toggle in shared header

--- maamara_market_project/Admin-Dashbord/src/cmponents/Auth/Routes/RoutesLayout/VendorLayout.jsx
+++ maamara_market_project/Admin-Dashbord/src/cmponents/Auth/Routes/RoutesLayout/VendorLayout.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import ProtectedRoute from "../ProtectRoute";
 import HeaderTop from "../../../Portions/HeaderTop/HeaderTop";
 import VendorDashboardNav from "../../../VENDORPAGE/Home/VendorDashboardNav";
 
-const VendorLayout = () => (
-  <ProtectedRoute requiredRole="vendor">
-    <div className="etsy-manager">
-      <HeaderTop />
-      <VendorDashboardNav />
-      <main className="etsy-manager-main">
-        <Outlet />
-      </main>
-    </div>
-  </ProtectedRoute>
-);
+const VendorLayout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <ProtectedRoute requiredRole="vendor">
+      <div className="etsy-manager">
+        <HeaderTop onMenuToggle={() => setSidebarOpen((open) => !open)} />
+        <VendorDashboardNav
+          open={sidebarOpen}
+          onClose={() => setSidebarOpen(false)}
+        />
+        <main className="etsy-manager-main">
+          <Outlet />
+        </main>
+      </div>
+    </ProtectedRoute>
+  );
+};
 
 export default VendorLayout;
