68a107b44f2cd32bcd38aa092ba7b40b608d2f6f
SakaKrib <sakahaoke@gmail.com>
2026-09-22 20:50:51 +0300

    Style and safely render rich blog HTML in cards

--- maamara_market_project/Admin-Dashbord/src/cmponents/VENDORPAGE/Blogs/BlogCard.jsx
+++ maamara_market_project/Admin-Dashbord/src/cmponents/VENDORPAGE/Blogs/BlogCard.jsx
@@ -83,7 +83,7 @@ export default function BlogCard({ post }) {
 
           {/* 🧠 CONTENT (SAFE RENDER) */}
           <div
-            className="text-gray-600 text-sm line-clamp-4"
+            className="blog-rich-text mt-2 text-sm leading-6 text-gray-700 [&_p]:mb-3 [&_h1]:mb-3 [&_h1]:text-2xl [&_h1]:font-bold [&_h2]:mb-3 [&_h2]:text-xl [&_h2]:font-bold [&_h3]:mb-2 [&_h3]:text-lg [&_h3]:font-semibold [&_ul]:mb-3 [&_ul]:list-disc [&_ul]:pl-5 [&_ol]:mb-3 [&_ol]:list-decimal [&_ol]:pl-5 [&_blockquote]:my-3 [&_blockquote]:border-l-4 [&_blockquote]:border-[#2563eb] [&_blockquote]:pl-4 [&_blockquote]:italic [&_a]:text-[#2563eb] [&_a]:underline"
             dangerouslySetInnerHTML={{
               __html: DOMPurify.sanitize(post.content || ""),
             }}
